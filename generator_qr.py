db4aeb1e28efb25f7d73c0066172844b8ebe52b0
Anwaar-Ahmad9212 <anwaarahmad9212@gmail.com>
2024-10-27 23:23:23 +0500

    Updated code

--- generator_qr.py
+++ generator_qr.py
@@ -1,27 +1,16 @@
-# import qrcode
-# from PIL import Image, ImageDraw,ImageFont
-
-# def qr_name(source, text):
-
-#     with Image.open(source) as img:
-#         width, height = img.size
-    
+import qrcode
+from PIL import Image, ImageDraw,ImageFont
 
 
-#     image = Image.open(source)
-#     draw_ = ImageDraw.draw(image)
-#     font = ImageFont.truetype("arial.ttf",50)
-#     text_pos()
-    
 
-# linkedin = qrcode.make("https://www.linkedin.com/in/muhammad-anwaar-ahmad/")
-# linkedin.save("linkedin.png")
+linkedin = qrcode.make("https://www.linkedin.com/in/muhammad-anwaar-ahmad/")
+linkedin.save("linkedin.png")
 
 
-# url_whatsapp = "https://api.whatsapp.com/send/?phone=923478661176&text=Asslamoalaikum%2C%0A+My+name+is+%5B%5D+.%0A+We+met+at+%5B%5D.+%0A+I+just+wanted+to+say+you+that%3A&type=phone_number&app_absent=0"
-# whatsapp = qrcode.make(url_whatsapp)
-# whatsapp.save("whatsapp.png")
+url_whatsapp = "https://api.whatsapp.com/send/?phone=923478661176&text=Asslamoalaikum%2C%0A+My+name+is+%5B%5D+.%0A+We+met+at+%5B%5D.+%0A+I+just+wanted+to+say+you+that%3A&type=phone_number&app_absent=0"
+whatsapp = qrcode.make(url_whatsapp)
+whatsapp.save("whatsapp.png")
 
 
-# instagram = qrcode.make("https://www.instagram.com/me_anwaarahmad/profilecard/?igsh=OG43bW9yajF5MDY5" )
-# instagram.save("instagram.png")
+instagram = qrcode.make("https://www.instagram.com/me_anwaarahmad/profilecard/?igsh=OG43bW9yajF5MDY5" )
+instagram.save("instagram.png")
